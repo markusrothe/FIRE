6bb538b1170ffc9d8451fb6587dbc6f9056ecdb4
Markus Rothe <markusrothe.mr@gmail.com>
2018-02-18 21:17:51 +0100

    Added further unittests for vertexDeclarations

--- test/vertexDeclarationTest.cpp
+++ test/vertexDeclarationTest.cpp
@@ -1,10 +1,36 @@
 #include "vertexDeclaration.h"
-
+#include <algorithm>
 #include <gtest/gtest.h>
 
-TEST(vertexDeclarationTest, DefaultConstructedDeclarationShouldHaveNoSection)
+namespace
 {
-    blocks::VertexDeclaration const vDecl;
-    ASSERT_TRUE(vDecl.GetSections().empty()); 
+    class VertexDeclarationTest : public ::testing::Test
+    {
+    public:
+        blocks::VertexDeclaration m_vDecl;
+    };
+
+} // namespace
+
+TEST_F(VertexDeclarationTest, DefaultConstructedDeclarationShouldHaveNoSection)
+{
+    EXPECT_TRUE(m_vDecl.GetSections().empty()); 
 }
 
+TEST_F(VertexDeclarationTest, AddingSectionsShouldIncreaseNumberOfSections)
+{
+    m_vDecl.AddSection("TEST", 2, 0, 3);
+    EXPECT_EQ(m_vDecl.GetSections().size(), 1);
+}
+
+TEST_F(VertexDeclarationTest, AddedSectionsShouldBeRetrievable)
+{
+    m_vDecl.AddSection("TEST", 2, 0, 3);
+
+    auto const& vDeclSections = m_vDecl.GetSections();
+
+    auto const foundSectionIt = std::find_if(std::begin(vDeclSections), std::end(vDeclSections)
+        , [](auto const& section) { return section.m_attributeName.compare("TEST") == 0; });
+
+    EXPECT_TRUE(std::end(vDeclSections) != foundSectionIt);
+}
