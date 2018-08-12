0c5ddf1016ff2981e705a21abe3611c2c2f25b95
Markus Rothe <markusrothe.mr@gmail.com>
2018-08-12 14:19:40 +0200

    Removed old unused code

--- src/old/materialMap.cpp
+++
@@ -1,9 +0,0 @@
-#include "materialMap.h"
-
-namespace blocks
-{
-    MaterialMapEntry CreateMaterialMapEntry(std::string const name, Material const& material)
-    {
-	return std::make_pair(name, material);
-    }
-}
