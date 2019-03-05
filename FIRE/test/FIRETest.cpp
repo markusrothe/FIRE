bf0ba6bffeb8e3dcc217ad6a3dc09418cbb5085e
Markus Rothe <markusrothe.mr@gmail.com>
2019-03-05 20:21:57 +0100

    Run gtest as post build step

--- FIRE/test/FIRETest.cpp
+++ FIRE/test/FIRETest.cpp
@@ -3,6 +3,12 @@
 
 TEST(FIRETest, Hello_FIRE_Test)
 {
-	FIRE::HelloWorld(); 
-	EXPECT_TRUE(true);
+	FIRE::HelloWorld();  
+	EXPECT_TRUE(true);   
 }
+
+TEST(FIRETest, Hello_2nd_FIRE_Test)
+{
+    FIRE::HelloWorld();
+    EXPECT_TRUE(true);
+} 
