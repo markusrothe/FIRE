55adec95e652fca3f9b6a20d01ae1cb697f507e4
Markus Rothe <markusrothe.mr@gmail.com>
2019-07-17 06:34:49 +0200

    try another fix war clang's false positive

--- FIRE/test/MeshManagerTest.cpp
+++ FIRE/test/MeshManagerTest.cpp
@@ -114,19 +114,15 @@ class MeshManagerCachingTest : public testing::TestWithParam<MeshCreationFunc>
 public:
     FIRE::MeshManager meshManager;
 };
+} // namespace
 
-// clang-format off
-SUPPRESS_UnusedFunction
-std::ostream& operator<<(std::ostream& os, MeshCreationFunc const&)
+static std::ostream& operator<<(std::ostream& os, MeshCreationFunc const&)
 {
     static int i = 0;
     os << i++;
     return os;
 }
-SUPPRESS_Pop
-// clang-format on
 
-} // namespace
 TEST_P(MeshManagerCachingTest, CachesACreatedMesh)
 {
     auto func = GetParam();
